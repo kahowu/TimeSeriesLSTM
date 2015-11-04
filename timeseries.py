4d369802022205abf5c288dad77e0371c0905d75
Jeff647 <jeffkahow892@gmail.com>
2015-11-04 18:15:02 -0500

    Added alibaba dataset and refactored the code

--- timeseries.py
+++ timeseries.py
@@ -6,16 +6,21 @@ from pybrain.structure.modules import LSTMLayer
 from pybrain.supervised import RPropMinusTrainer
 from sys import stdout
 import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+import numpy as np
 
 # Dummy data 
 def create_data (): 
-	data = [1] * 3 + [2] * 3
-	data *= 3
+	data = [1] * 10 + [2] * 10 + [3] * 10 + [4] * 10 + [5] * 10 
+	data *= 10
 
 	# Put this timeseries into a supervised dataset, where the target for each sample is the next sample
 	ds = SequentialDataSet(1, 1)
 	for sample, next_sample in zip(data, cycle(data[1:])):
 	    ds.addSample(sample, next_sample)
+
+	print("Done data preparation...")
 
 	return ds 
 
@@ -28,7 +33,9 @@ def train (ds, net):
 	EPOCHS = EPOCHS_PER_CYCLE * CYCLES
 	for i in xrange(CYCLES):
 	    trainer.trainEpochs(EPOCHS_PER_CYCLE)
-	    train_errors.append(trainer.testOnData())
+	    error = trainer.testOnData()
+	    train_errors.append(error)
+	    print (error)
 	    epoch = (i+1) * EPOCHS_PER_CYCLE
 	    print("\r epoch {}/{}".format(epoch, EPOCHS))
 	    stdout.flush()
@@ -51,11 +58,26 @@ def plot (train_errors, EPOCHS, EPOCHS_PER_CYCLE):
 	plt.ylabel('error')
 	plt.show()
 
+def create_trade_data (data):
+	ds = SequentialDataSet(1, 1)
+	for sample, next_sample in zip(data, cycle(data[1:])):
+	 	ds.addSample(sample, next_sample)	
+
+	return ds 
+
+
 
 if __name__ == "__main__":
+	# Create dummy data 
+	# ds = create_data()
 
-	# Create dummy data
-	ds = create_data()
+	# Load data 
+	arg = sys.argv
+	filename = arg[1]
+	df = pd.read_csv(filename)
+	data = df["Open"]
+	ds = create_trade_data (data)
+	print (ds) 
 
 	# Build a simple LSTM network with 1 input node, 5 LSTM cells and 1 output node
 	net = buildNetwork(1, 5, 1, hiddenclass=LSTMLayer, outputbias=False, recurrent=True)
@@ -68,4 +90,7 @@ if __name__ == "__main__":
 
 	# print("Predict next sample")
 	# predict (ds, net)
+	# print (np.mean(ds["target"]))
+
+
 
